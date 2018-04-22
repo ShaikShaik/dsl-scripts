4a565f18fb83878ced7e7b50b07537df4f29e06b
ShaikShaik <rafi.shaikk@gmail.com>
2018-04-22 18:34:16 +0530

    Update dsl_pipeline.groovy

--- dsl_pipeline.groovy
+++ dsl_pipeline.groovy
@@ -1,52 +1,52 @@
-folder('PIPELINE_JOB/CODEADDAGURU') {
+folder('PIPELINE_JOB/DEVOPSPROJECT') {
         description('hpsim project foloder created')
 }
-freeStyleJob('PIPELINE_JOB/CODEADDAGURU/compile') {
+freeStyleJob('PIPELINE_JOB/DEVOPSPROJECT/compile') {
     logRotator(-1, 10)
     scm {
-        github('NagireddyGuduru/myweb', 'master')
+        github('ShaikShaik/myweb', 'master')
     }
     steps {
         maven('clean compile')
     }
     publishers {
-        downstream('PIPELINE_JOB/CODEADDAGURU/test', 'SUCCESS')
+        downstream('PIPELINE_JOB/DEVOPSPROJECT/test', 'SUCCESS')
     }
 }
-mavenJob('PIPELINE_JOB/CODEADDAGURU/test') {
+mavenJob('PIPELINE_JOB/DEVOPSPROJECT/test') {
     logRotator(-1, 10)
      scm {
-        github('NagireddyGuduru/myweb', 'master')
+        github('ShaikShaik/myweb', 'master')
     }
     goals('clean test')
    
    publishers {
-        downstream('PIPELINE_JOB/CODEADDAGURU/sonar', 'SUCCESS')
+        downstream('PIPELINE_JOB/DEVOPSPROJECT/sonar', 'SUCCESS')
     }
 }
-mavenJob('PIPELINE_JOB/CODEADDAGURU/sonar') {
+mavenJob('PIPELINE_JOB/DEVOPSPROJECT/sonar') {
     logRotator(-1, 10)
      scm {
-        github('NagireddyGuduru/myweb', 'master')
+        github('ShaikShaik/myweb', 'master')
     }
     goals('clean sonar:sonar')
   publishers {
-        downstream('PIPELINE_JOB/CODEADDAGURU/nexus', 'SUCCESS')
+        downstream('PIPELINE_JOB/DEVOPSPROJECT/nexus', 'SUCCESS')
     }
 }
-mavenJob('PIPELINE_JOB/CODEADDAGURU/nexus') {
+mavenJob('PIPELINE_JOB/DEVOPSPROJECT/nexus') {
     logRotator(-1, 10)
      scm {
-        github('NagireddyGuduru/myweb', 'master')
+        github('ShaikShaik/myweb', 'master')
     }
     goals('clean deploy')
 }
-buildPipelineView('PIPELINE_JOB/CODEADDAGURU/build-pipeline') {
+buildPipelineView('PIPELINE_JOB/DEVOPSPROJECT/build-pipeline') {
     filterBuildQueue()
     filterExecutors()
     
     displayedBuilds(5)
-    selectedJob('PIPELINE_JOB/CODEADDAGURU/compile')
+    selectedJob('PIPELINE_JOB/DEVOPSPROJECT/compile')
     alwaysAllowManualTrigger()
     showPipelineParameters()
     refreshFrequency(60)
